dc927214079f630f20b846d493000b1733a2ceed
Francesco Pirrotta <fpirrotta@topcon.com>
2017-04-21 18:42:30 +0200

    Define as LoggerInstance the fixtures for test

--- test/WinstonLoggerSpec.ts
+++ test/WinstonLoggerSpec.ts
@@ -3,7 +3,7 @@ import * as TypeMoq from "typemoq";
 import { LogLevel } from "prettygoat";
 import { LoggerInstance } from "winston";
 import expect = require("expect.js");
-import WinstonLogger from "../scripts/logger/WinstonLogger";
+import WinstonLogger from "../scripts/WinstonLogger";
 import MockLoggerInstance from "./fixtures/MockLoggerInstance";
 
 describe("Given a WinstonLogger", () => {
@@ -12,13 +12,13 @@ describe("Given a WinstonLogger", () => {
     let logger: TypeMoq.IMock<LoggerInstance>;
 
     beforeEach(() => {
-        logger = TypeMoq.Mock.ofInstance<LoggerInstance>((new MockLoggerInstance() as any));
+        logger = TypeMoq.Mock.ofInstance<LoggerInstance>(new MockLoggerInstance());
         logger.setup(l => l.log(TypeMoq.It.isAnyString(), TypeMoq.It.isAnyString(), TypeMoq.It.isAny())).returns(() => null);
         subject = new WinstonLogger(logger.object);
     });
 
-    context("when ask to log a message", () => {
-        it("should demand the log to the winston logger with the defined level", () => {
+    context("when asked to log a message", () => {
+        it("should request the wiston logger to log with the defined level", () => {
             subject.debug("A debug Message");
             logger.verify(l => l.log("debug", "A debug Message"), TypeMoq.Times.once());
             subject.info("An info Message");
@@ -31,14 +31,14 @@ describe("Given a WinstonLogger", () => {
     });
 
     context("when a valid log level is set", () => {
-        it("should set the corret log level into winston logger", () => {
+        it("should set the correct log level into winston logger", () => {
             subject.setLogLevel(LogLevel.Info);
             expect(subject.getLogLevel()).to.be(LogLevel.Info);
         });
     });
 
     context("when an invalid log level is set", () => {
-        it("should set the 'DEBUG' log level into winston logger", () => {
+        it("should set the 'DEBUG' log level", () => {
             subject.setLogLevel(-1);
             expect(subject.getLogLevel()).to.be(LogLevel.Debug);
         });
